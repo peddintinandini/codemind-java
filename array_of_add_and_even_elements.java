7494c535c685c924c87f34168097e8117fa4d90a
peddintinandini <96322585+peddintinandini@users.noreply.github.com>
2022-05-29 08:14:03 +0530

    import java.util.Scanner; class Teja {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,m=1;         n=sc.nextInt();         int x[]=new int[n];         for(i=0;i<n;i++)         x[i]=sc.nextInt();         for(i=0;i<n;i++)         if(x[i]%2!=0)         {             System.out.printf("%d ",x[i]);         }         for(i=0;i<n;i++)         if(x[i]%2==0)         {             System.out.printf("%d ",x[i]);         }     } }

---
+++ array_of_add_and_even_elements.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Teja
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,m=1;
+        n=sc.nextInt();
+        int x[]=new int[n];
+        for(i=0;i<n;i++)
+        x[i]=sc.nextInt();
+        for(i=0;i<n;i++)
+        if(x[i]%2!=0)
+        {
+            System.out.printf("%d ",x[i]);
+        }
+        for(i=0;i<n;i++)
+        if(x[i]%2==0)
+        {
+            System.out.printf("%d ",x[i]);
+        }
+    }
+}
