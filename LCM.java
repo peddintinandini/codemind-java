cb2f1cc15a3706c68a7a2bea49db54a77fdff03d
peddintinandini <96322585+peddintinandini@users.noreply.github.com>
2022-05-29 17:02:19 +0530

    import java.util.Scanner; class teja {     public static void main(String args[])     {         int n1,n2,gcd=1,lcm,i;         Scanner sc=new Scanner(System.in);
            n1=sc.nextInt();
            n2=sc.nextInt();
            for(i=1;i<=n1&&i<=n2;i++)
            {
                if(n1%i==0&&n2%i==0)
            gcd=i;
            }
            lcm=(n1*n2)/gcd;
            System.out.println(lcm);
        }
    }

---
+++ LCM.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class teja
+{
+    public static void main(String args[])
+    {
+        int n1,n2,gcd=1,lcm,i;
+        Scanner sc=new Scanner(System.in);
+        
+        n1=sc.nextInt();
+        n2=sc.nextInt();
+        for(i=1;i<=n1&&i<=n2;i++)
+        {
+            if(n1%i==0&&n2%i==0)
+        gcd=i;
+        }
+        lcm=(n1*n2)/gcd;
+        System.out.println(lcm);
+    }
+}
