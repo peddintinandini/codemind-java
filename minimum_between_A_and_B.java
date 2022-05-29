8f7b4bb910bd0013e5b7e0e201edb7ce2567c492
peddintinandini <96322585+peddintinandini@users.noreply.github.com>
2022-05-29 07:42:23 +0530

    import java.util.Scanner; class Teja {     public static void main(String args[])     {         int n,i;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int x[]=new int[n];         for(i=0;i<n;i++)         x[i]=sc.nextInt();         int a,y;         a=sc.nextInt();         y=sc.nextInt();         int max=-1;         for(i=0;i<n;i++)         if(x[i]>=a&&x[i]<y)         if(x[i]>max)         max=x[i];         int min=max;         for(i=0;i<n;i++)          if(x[i]>=a&&x[i]<=y)         if(x[i]<min)         min=x[i];         System.out.println(min);     } }

---
+++ minimum_between_A_and_B.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class Teja
+{
+    public static void main(String args[])
+    {
+        int n,i;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int x[]=new int[n];
+        for(i=0;i<n;i++)
+        x[i]=sc.nextInt();
+        int a,y;
+        a=sc.nextInt();
+        y=sc.nextInt();
+        int max=-1;
+        for(i=0;i<n;i++)
+        if(x[i]>=a&&x[i]<y)
+        if(x[i]>max)
+        max=x[i];
+        int min=max;
+        for(i=0;i<n;i++)
+         if(x[i]>=a&&x[i]<=y)
+        if(x[i]<min)
+        min=x[i];
+        System.out.println(min);
+    }
+}
