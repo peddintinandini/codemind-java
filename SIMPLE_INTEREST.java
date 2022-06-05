90d97a32aea1c14d1c176d8df2e189e41c8f851f
peddintinandini <96322585+peddintinandini@users.noreply.github.com>
2022-06-05 20:21:20 +0530

    import java.util.Scanner; class teja {     public static void main(String args[])     {         int p,t,r,k;         Scanner sc= new Scanner(System.in);         p=sc.nextInt();          t=sc.nextInt();           r=sc.nextInt();           k=(p*r*t)/100;           System.out.println(k);
        }
    }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class teja
+{
+    public static void main(String args[])
+    {
+        int p,t,r,k;
+        Scanner sc= new Scanner(System.in);
+        p=sc.nextInt();
+         t=sc.nextInt();
+          r=sc.nextInt();
+          k=(p*r*t)/100;
+          System.out.println(k);
+        
+    }
+}
