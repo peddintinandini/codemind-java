bdff3382daabb3b58f7b041c1a3850c1f358a814
peddintinandini <96322585+peddintinandini@users.noreply.github.com>
2022-07-09 11:33:53 +0530

    import java.util.Scanner; class abs {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,s=0;         n=sc.nextInt();         int a[]=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();             if(a[i]%2!=0)             {                 s=s+a[i];             }         }
            System.out.println(s);
        }
    }

---
+++ ODD_Sum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class abs
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,s=0;
+        n=sc.nextInt();
+        int a[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+            if(a[i]%2!=0)
+            {
+                s=s+a[i];
+            }
+        }
+           
+        System.out.println(s);
+    }
+}
