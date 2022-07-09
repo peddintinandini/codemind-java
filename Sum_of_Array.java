e6822790c067ef40c30a2a1c3bd88a63fc24ea16
peddintinandini <96322585+peddintinandini@users.noreply.github.com>
2022-07-09 11:36:18 +0530

    import java.util.Scanner; class abs {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,s=0;         n=sc.nextInt();         int a[]=new int[n];         for(i=0;i<n;i++)         {            a[i]=sc.nextInt();                 s=s+a[i];
            }
               
            System.out.println(s);
        }
    }

---
+++ Sum_of_Array.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class abs
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,s=0;
+        n=sc.nextInt();
+        int a[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+           a[i]=sc.nextInt();
+                s=s+a[i];
+            
+        }
+           
+        System.out.println(s);
+    }
+}
