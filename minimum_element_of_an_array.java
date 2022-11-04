0f25ac2cb7eeca7591615a95805ed00438f38d32
peddintinandini <96322585+peddintinandini@users.noreply.github.com>
2022-11-04 07:14:21 +0530

    import java.util.Scanner; class teja {     public static void main(String args[])     {         int n,i,temp;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int a[]=new int[n];         for (i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         int min=a[0];         for (i=0;i<n;i++)         {             if (a[i]<min)             {               temp=a[i];               min=temp;
                }
            }
            System.out.print(min);
        }
    }

---
+++ minimum_element_of_an_array.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class teja
+{
+    public static void main(String args[])
+    {
+        int n,i,temp;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int a[]=new int[n];
+        for (i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int min=a[0];
+        for (i=0;i<n;i++)
+        {
+            if (a[i]<min)
+            {
+              temp=a[i];
+              min=temp;
+              
+            }
+        }
+        System.out.print(min);
+    }
+}
