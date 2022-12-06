bc61f1a24a5d953fc6c0ca0c2cf2d1bc1107ac4c
peddintinandini <96322585+peddintinandini@users.noreply.github.com>
2022-12-06 21:30:43 +0530

    import java.util.Scanner; class nandu {     public int prime(int n)     {         int i,c=0;         if (n==1)         {             c=1;         }         for(i=2;i<(int)Math.sqrt(n)+1;i++)         {             if(n%i==0)             {                 c+=1;             }         }         return c;     }     public int pal(int n){         int r,rev=0,temp;         while (n>0)         {             r=n%10;             rev=rev*10+r;             n=n/10;
            }
            return rev;
        }
        public static void main(String args[])
        {
            Scanner sc=new Scanner(System.in);
            nandu na=new nandu();
            int n,r,c2=0,c1=0;
            n=sc.nextInt();
            if(na.prime(n)==0)
            {
                if(na.prime(na.pal(n))==0)
                {
                    System.out.println("circular prime");
                }
                else
                {
                    System.out.println("prime but not a circular prime");
                }
            }
                else
                {
                    System.out.println("not prime");
                }
          
            
                
          
            
        }
    }

---
+++ circular_prime.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+class nandu
+{
+    public int prime(int n)
+    {
+        int i,c=0;
+        if (n==1)
+        {
+            c=1;
+        }
+        for(i=2;i<(int)Math.sqrt(n)+1;i++)
+        {
+            if(n%i==0)
+            {
+                c+=1;
+            }
+        }
+        return c;
+    }
+    public int pal(int n){
+        int r,rev=0,temp;
+        while (n>0)
+        {
+            r=n%10;
+            rev=rev*10+r;
+            n=n/10;
+            
+        }
+        return rev;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        nandu na=new nandu();
+        int n,r,c2=0,c1=0;
+        n=sc.nextInt();
+        if(na.prime(n)==0)
+        {
+            if(na.prime(na.pal(n))==0)
+            {
+                System.out.println("circular prime");
+            }
+            else
+            {
+                System.out.println("prime but not a circular prime");
+            }
+        }
+            else
+            {
+                System.out.println("not prime");
+            }
+      
+        
+            
+      
+        
+    }
+}
